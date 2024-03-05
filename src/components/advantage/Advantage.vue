f88f02ed36da5a9c377edd63d39e745f13836061
tranledong <tranledong0101@gmail.com>
2024-03-05 17:26:13 +0700

    First commit working day 05.03

---
+++ src/components/advantage/Advantage.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template></template>
+
+<style scoped>
+/* Desktop */
+@media screen {
+}
+
+/* Tablet */
+@media screen and (max-width: 1170px) {
+}
+
+/* Mobile */
+@media screen and (max-width: 480px) {
+}
+</style>
